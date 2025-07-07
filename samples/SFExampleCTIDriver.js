67423544be41dc1d1d3d6618454c4597222d4d63
Adekola Adebayo <adekola@users.noreply.github.com>
2025-07-07 12:26:39 +0200

    Update SFExampleCTIDriver.js
    Signed-off-by: Adekola Adebayo <adekola@users.noreply.github.com>

--- samples/SFExampleCTIDriver.js
+++ samples/SFExampleCTIDriver.js
@@ -1,1 +1,91 @@
-(()=>{"use strict";(()=>{class e{constructor(){this.conversationStartTimes={},this.callerNumbers={}}initialize(){if(void 0===window.sforce||void 0===window.sforce.opencti){const o="/support/api/54.0/lightning/opencti.js",n=`${new URL(document.referrer).origin}${o}`;return e.loadScript(n)}return Promise.resolve(!0)}bindEvents(){var e,o;const n=null===(o=null===(e=window.Microsoft)||void 0===e?void 0:e.CCaaS)||void 0===o?void 0:o.EmbedSDK;n&&(n.conversation.onConversationLoaded(e=>{console.log("Conversation loaded:",e),console.log("Available properties:",Object.keys(e)),console.log("customerPhoneNumber:",e.customerPhoneNumber),console.log("customerName:",e.customerName);const{liveWorkItemId:o,customerPhoneNumber:n}=e;console.log(e),this.conversationStartTimes[o]=new Date,this.callerNumbers[o]=n||"",console.log("Stored phone number:",this.callerNumbers[o])}),n.conversation.onStatusChange(e=>{const{liveWorkItemId:o,statusCode:n}=e;if(console.log("Status update received:",e),console.log(`Status code for liveWorkItemId ${o}:`,n),5===n||4===n){const e=this.conversationStartTimes[o],n=new Date,t={Subject:"Call Summary",callType:"Inbound",callDurationInSeconds:Math.floor((n.getTime()-e.getTime())/1e3),ContactMobile:this.callerNumbers[o],entityApiName:"Case",CaseNumber:""};console.log("Log data to be saved:",t),window.sforce.opencti.saveLog({value:t,callback:e=>{e.success?console.log("Log saved successfully:",e):console.error("Failed to save log:",e.errors||"An unknown error occurred.")}})}}))}static loadScript(e){return new Promise((o,n)=>{const t=document.createElement("script");t.type="text/javascript",t.async=!0,t.src=e,t.onload=function(){o(!0)},t.onerror=function(){n(new Error(`Error in loading ${e}`))},document.getElementsByTagName("head")[0].appendChild(t)})}}window.CCaaS=window.CCaaS||{},window.CCaaS.CTIDriver||(window.CCaaS.CTIDriver=e)})()})();
+(() => {
+  "use strict";
+  (() => {
+    class e {
+      constructor() {
+        (this.conversationStartTimes = {}), (this.callerNumbers = {});
+      }
+      initialize() {
+        if (void 0 === window.sforce || void 0 === window.sforce.opencti) {
+          const o = "/support/api/54.0/lightning/opencti.js",
+            t = `${new URL(document.referrer).origin}${o}`;
+          return e.loadScript(t);
+        }
+        return Promise.resolve(!0);
+      }
+      bindEvents() {
+        var e, o;
+        const t =
+          null ===
+            (o =
+              null === (e = window.Microsoft) || void 0 === e
+                ? void 0
+                : e.CCaaS) || void 0 === o
+            ? void 0
+            : o.EmbedSDK;
+        t &&
+          (t.conversation.onConversationLoaded((e) => {
+            console.log("Conversation loaded:", e),
+              console.log("Available properties:", Object.keys(e)),
+              console.log("customerPhoneNumber:", e.customerPhoneNumber),
+              console.log("customerName:", e.customerName);
+            const { liveWorkItemId: o, customerPhoneNumber: t } = e;
+            console.log(e),
+              (this.conversationStartTimes[o] = new Date()),
+              (this.callerNumbers[o] = t || ""),
+              console.log("Stored phone number:", this.callerNumbers[o]);
+          }),
+          t.conversation.onStatusChange((e) => {
+            const { liveWorkItemId: o, statusCode: t } = e;
+            if (
+              (console.log("Status update received:", e),
+              console.log(`Status code for liveWorkItemId ${o}:`, t),
+              5 === t || 4 === t)
+            ) {
+              const e = this.conversationStartTimes[o],
+                t = new Date(),
+                n = {
+                  Subject: "Call Summary",
+                  callType: "Inbound",
+                  Call_Duration__c: Math.floor(
+                    (t.getTime() - e.getTime()) / 1e3
+                  ),
+                  ContactPhone: this.callerNumbers[o],
+                  entityApiName: "Case",
+                  CaseNumber: "",
+                };
+              console.log("Log data to be saved:", n),
+                window.sforce.opencti.saveLog({
+                  value: n,
+                  callback: (e) => {
+                    e.success
+                      ? console.log("Log saved successfully:", e)
+                      : console.error(
+                          "Failed to save log:",
+                          e.errors || "An unknown error occurred."
+                        );
+                  },
+                });
+            }
+          }));
+      }
+      static loadScript(e) {
+        return new Promise((o, t) => {
+          const n = document.createElement("script");
+          (n.type = "text/javascript"),
+            (n.async = !0),
+            (n.src = e),
+            (n.onload = function () {
+              o(!0);
+            }),
+            (n.onerror = function () {
+              t(new Error(`Error in loading ${e}`));
+            }),
+            document.getElementsByTagName("head")[0].appendChild(n);
+        });
+      }
+    }
+    (window.CCaaS = window.CCaaS || {}),
+      window.CCaaS.CTIDriver || (window.CCaaS.CTIDriver = e);
+  })();
+})();
